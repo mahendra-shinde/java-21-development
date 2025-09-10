7c4ecdec296d457917e927768adb3e095b2c2083
Mahendra shinde <MahendraShinde@synergetics-india.com>
2025-09-10 15:38:58 +0530

    Refactor Main3 to use Lambda expressions for Callable tasks instead of FactorialTask class

--- demos/demo10/src/demo10/Main3.java
+++ demos/demo10/src/demo10/Main3.java
@@ -12,19 +12,21 @@ public class Main3 {
 	public static void main(String[] args) {
 		ExecutorService executor = Executors.newFixedThreadPool(2);
 		Future<Double> result1 = executor.submit(new FactorialTask(4));
-		Future<Double> result2 = executor.submit(new FactorialTask(5));
-		
+		// No need for FactorialTask class, we can use Lambda to create Callable
+		// directly
+		Future<Double> result2 = executor.submit(() -> calcFactorial(5));
+
 		try {
-		System.out.println("Factorial of 4 is : "+ result1.get(10,TimeUnit.SECONDS));
-		System.out.println("Factorial of 5 is : "+ result2.get(10, TimeUnit.SECONDS));
-		}catch(TimeoutException ex2) {
+			System.out.println("Factorial of 4 is : " + result1.get(10, TimeUnit.SECONDS));
+			System.out.println("Factorial of 5 is : " + result2.get(10, TimeUnit.SECONDS));
+		} catch (TimeoutException ex2) {
 			System.out.println("Timed out !");
-		}catch(InterruptedException ex3) {
+		} catch (InterruptedException ex3) {
 			System.out.println("Interrupted !!!");
-		}catch(ExecutionException ex) {
+		} catch (ExecutionException ex) {
 			ex.printStackTrace();
 		}
-		
+
 		try {
 			executor.awaitTermination(10, TimeUnit.SECONDS);
 			executor.shutdown();
@@ -32,8 +34,17 @@ public class Main3 {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
 
+	}
+
+	// A Simple method which could be converted into "Callable" using Lambda
+	private static Double calcFactorial(double num) {
+		double factorial = 1;
+		while (num >= 1) {
+			factorial *= num;
+			num--;
+		}
+		return factorial;
 	}
 
 }
